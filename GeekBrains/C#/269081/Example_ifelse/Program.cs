72029ff8997a2810a145d4bdfba49b04d12688fb
SC <sc0372q@gmail.com>
2022-10-11 23:19:27 +0300

    IfElse. Первое использование

---
+++ GeekBrains/C#/269081/Example_ifelse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Николя")
+{
+    Console.WriteLine("Дратути, Николя!");
+}
+else
+    Console.WriteLine($"Привет, {username}!");
