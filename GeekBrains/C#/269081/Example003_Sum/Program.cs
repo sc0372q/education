6790790652325e85f537ce1460325b4362ae8bfc
SC <sc0372q@gmail.com>
2022-10-11 22:57:02 +0300

    Random().Next(). Сложение двух чисел

--- GeekBrains/C#/269081/Example003_Sum/Program.cs
+++ GeekBrains/C#/269081/Example003_Sum/Program.cs
@@ -1,3 +1,3 @@
-﻿int numberA = 3;
-int numberB = 4;
+﻿int numberA = new Random().Next(0, 10); // случайное чиисло от 0 до 9
+int numberB = new Random().Next(0, 10); // случайное чиисло от 0 до 9
 Console.WriteLine(numberA + numberB);
